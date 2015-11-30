de359419ab4b194100e79365bfe15b401f762131
Darek Rossman <darekrossman@me.com>
2015-11-30 11:35:43 -0500

    add test for detect device

--- src/server/middleware/detectDevice.js
+++ src/server/middleware/detectDevice.js
@@ -1,65 +1,18 @@
 import device from 'device';
 
-export default function deviceDetect() {
-  return async function (req, res, next) {
-    try {
-      if (req.session.device) {
+export default function() {
+  return async (req, res, next) => {
+    if (req.session && req.session.device) {
       req.device = req.session.device;
       next();
-      } else {
-        let userAgent = req.headers['user-agent'];
-        if (!userAgent) next();
-        const deviceObject = device(userAgent);
-        req.session.device = req.device = deviceObject;
-        next();
-      }
-    } catch(err) {
-      next(err);
+    } else {
+      let userAgent = req.headers['user-agent'];
+      if (!userAgent) next();
+      const deviceObject = device(userAgent);
+      if (req.session)
+        req.session.device = deviceObject;
+      req.device = deviceObject;
+      next();
     }
   };
 }
-
-// export default function deviceDetect() {
-//   return async function (req, res, next) {
-//     try {
-//       if (req.session.device) {
-//       req.device = req.session.device;
-//       next();
-//       } else {
-//         let userAgent = req.headers['user-agent'];
-//         if (!userAgent) next();
-//         const deviceString = await getWURFLData(userAgent);
-//         const deviceObject = parseDeviceString(deviceString);
-//         req.session.device = req.device = deviceObject;
-//         next();
-//       }
-//     } catch(err) {
-//       next(err);
-//     }
-//   };
-// }
-//
-// async function getWURFLData(userAgent) {
-//     const response = await fetch('http://wurfl.io/wurfl.js', {
-//       headers: {
-//         'user-agent': userAgent
-//       }
-//     });
-//     return await response.text();
-// }
-//
-// function parseDeviceString(text) {
-//   let wurflDeviceFn = text
-//     .substr(text.indexOf('eval'))
-//     .replace(/^eval/, '');
-//
-//   let deviceString = eval(wurflDeviceFn).replace('var WURFL=','');
-//
-//   let wurflDevice = JSON.parse(
-//     deviceString
-//       .replace('var WURFL=','')
-//       .substr(0, deviceString.indexOf(';'))
-//   );
-//
-//   return wurflDevice;
-// }
--- src/server/middleware/detectGeo.js
+++ src/server/middleware/detectGeo.js
@@ -3,16 +3,20 @@ import network from 'network';
 export default function() {
   return async (req, res, next) => {
     const uri = 'https://api.valpak.com/pub/geo/iplocations/';
-    if (req.cookies.geo) {
-      req.geo = req.cookies.geo
-      next()
+    if (req.cookies && req.cookies.geo) {
+      req.geo = req.cookies.geo;
+      next();
     } else {
       network.get_public_ip(async (err, ip) => {
-        let response = await fetch(uri + ip);
-        let geo = await response.json()
-        res.cookie('geo', geo)
-        req.geo = geo;
-        next();
+        try {
+          let response = await fetch(uri + ip);
+          let geo = await response.json();
+          res.cookie('geo', geo);
+          req.geo = geo;
+          next();
+        } catch (e) {
+          next(e);
+        }
       });
     }
   };
